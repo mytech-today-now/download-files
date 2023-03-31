49f4e598666b504d213351488c03a70b0b9dcbd8
Kyle Rode <kyle@mytech.today>
2023-03-31 18:45:28 -0500

    modified:   JsonUrl.json 	modified:   download-files.ps1

--- download-files.ps1
+++ download-files.ps1
@@ -1,4 +1,5 @@
 <#
+
 .SYNOPSIS
 This is a PowerShell script that downloads files from a JSON file using the URLs provided in the JSON, and saves them in a specified directory.
 
@@ -21,8 +22,8 @@ The URL of the JSON file containing the download URLs.  JsonUrl file in the form
             "Url": "https://insidiousmeme.com/presenta/ai/powershell/ChatGPT.url"
         },
         {
-            "Name": "yakGPT.ico\",
-            "Url": \"https://insidiousmeme.com/presenta/ai/powershell/yakGPT.ico"
+            "Name": "yakGPT.ico",
+            "Url": "https://insidiousmeme.com/presenta/ai/powershell/yakGPT.ico"
         },
         {
             "Name": "YakGPT.url",
@@ -34,14 +35,14 @@ The URL of the JSON file containing the download URLs.  JsonUrl file in the form
         },
         {
             "Name": "myTech.Today Tools.url",
-            "Url\": "https://insidiousmeme.com/presenta/ai/powershell/myTech.TodayTools.url"
+            "Url": "https://insidiousmeme.com/presenta/ai/powershell/myTech.TodayTools.url"
         },
         {
             "Name": "myTechToday-portrait-02.ico",
-            "Url\": \"https://insidiousmeme.com/presenta/ai/powershell/myTechToday-portrait-02.ico"
+            "Url": "https://insidiousmeme.com/presenta/ai/powershell/myTechToday-portrait-02.ico"
         },
         {
-            "Name": "myTech.Today.url\",
+            "Name": "myTech.Today.url",
             "Url": "https://insidiousmeme.com/presenta/ai/powershell/myTech.Today.url"
         }
     ]
@@ -60,72 +61,96 @@ param (
     [string]$JsonUrl
 )
 
-$today = Get-Date -format "yyyy-MM-dd"
-
-# Set the default output directory to the programs directory or the value of the environmental variable
-$programDir = "$env:Programs\myTechToday\downloads\"
-if ([Environment]::GetEnvironmentVariable("myTechToday")) {
-    $customDir = [Environment]::GetEnvironmentVariable("myTechToday") + "\downloads\"
-}
-
-# Check if custom directory exists and set output directory accordingly
-if ($customDir -and (Test-Path -Path $customDir)) {
-    $OutputDir = Join-Path $customDir $today
-} elseif (Test-Path -Path $programDir) {
-    $OutputDir = Join-Path $programDir $today
-} else {
-    Write-Warning "Neither custom nor default directory found. Using current working directory."
-    $OutputDir = Get-Location
-}# If the download directory does not exist, create it
-if (!(Test-Path -Path $OutputDir)) {
-    Write-Verbose "Creating directory: $OutputDir"
-    New-Item -ItemType Directory -Path $OutputDir
-}
-
 try {
-    # Check if the web server is accessible, Check if the $json object is null or if it does not contain files array. 
+
+    # Get the date in yyyy-mm-dd format.
+    $today = Get-Date -Format "yyyy-MM-dd"
+
+    # Set the default output directory to the programs directory or the value of the environmental variable.
+    $programDir = "$env:Programs\myTechToday\downloads\"
+    if ([Environment]::GetEnvironmentVariable("myTechToday")) {
+        $customDir = [Environment]::GetEnvironmentVariable("myTechToday") + "\downloads\"
+    }
+
+    # Check if custom directory exists and set output directory accordingly.
+    if ($customDir -and (Test-Path -Path $customDir)) {
+        $OutputDir = Join-Path $customDir $today
+    } elseif (Test-Path -Path $programDir) {
+        $OutputDir = Join-Path $programDir $today
+    } else {
+        Write-Warning "Neither custom nor default directory found. Using current working directory."
+        $OutputDir = "$env:MyDocuments\myTechToday\downloads\"
+    }
+
+    # If the download directory does not exist, create it.
+    if (!(Test-Path -Path $OutputDir)) {
+        Write-Verbose "Creating directory: $OutputDir"
+        New-Item -ItemType Directory -Path $OutputDir
+    }
+
+    # Get the JSON file from the specified URL.
     $json = Invoke-RestMethod $JsonUrl
+
+    # Check if the $json object is null or if it does not contain files array. 
     if (!$json.Files) {
         Write-Error "Error: JSON file may be empty or incorrectly formatted."
     }
     else {
+
+        # Loop through each file object in the JSON file.
         foreach ($file in $json.Files) {
+
             $fileName = $file.Name
             $fileUrl = $file.Url
             $fileOutputPath = Join-Path $OutputDir $fileName
             
-            # Test if the file exists on the internet
+            # Test if the file exists on the internet.
             $fileAvailable = $true
-            try {                
+            try {
                 $null = Invoke-WebRequest $fileUrl -UseBasicParsing -ErrorAction Stop
             } catch {
+                $errorMessage = "Error: File $fileName not found on $fileUrl."
+                Write-Warning $errorMessage
+                Write-Output $errorMessage
                 $fileAvailable = $false
             }
 
-            # If the file exists on the internet, download it, otherwise print a warning message
-            if ($fileAvailable) {                
-                # Test if the file already exists
-                if (!(Test-Path -Path $fileOutputPath)) {
-                    # Download file to $OutputDir.                    
-                    Invoke-WebRequest $fileUrl -OutFile $fileOutputPath
-                    # Test and validate the file after downloading to ensure it was downloaded successfully
-                    if (!(Test-Path -Path $fileOutputPath)) {
-                        Write-Warning "Download of $fileUrl to $fileOutputPath failed!"
-                    } else {
-                        Write-Verbose "Downloaded $fileName successfully from $fileUrl to $fileOutputPath"
-                    }                } else {
+            # If the file exists on the internet, download it, otherwise print a warning message.
+            if ($fileAvailable) {
+                
+                # Test if the file already exists.
+                if (Test-Path -Path $fileOutputPath) {
                     Write-Warning "File $fileName already exists in $OutputDir"
+                    continue
                 }
-            } else {
-                Write-Warning "File $fileName unavailable for download from $fileUrl. Skipping...\\\"
-            }
+
+                # Download file to $OutputDir.
+                try {
+                    Invoke-WebRequest $fileUrl -OutFile $fileOutputPath -ErrorAction Stop
+                    Write-Verbose "Downloaded $fileName successfully from $fileUrl to $fileOutputPath"
+                } catch {
+                    $errorMessage = "Error: Failed to download $fileUrl to $fileOutputPath."
+                    Write-Warning $errorMessage
+                    Write-Output $errorMessage
+                    continue
+                }
+
+                # Test and validate the file after downloading to ensure it was downloaded successfully.
+                if (!(Test-Path -Path $fileOutputPath)) {
+                    $errorMessage = "Error: Download of $fileUrl to $fileOutputPath failed!"
+                    Write-Warning $errorMessage
+                    Write-Output $errorMessage
+                } 
+            } 
         }
     }
+
 } catch {
-    Write-Error "An error occurred: $($Error[0].Exception.Message)"
+    $errorMessage = "An error occurred: $($Error[0].Exception.Message)"
+    Write-Error $errorMessage
+    Write-Output $errorMessage
     exit 1
 }
 
 # Return success.
 exit 0
-```
