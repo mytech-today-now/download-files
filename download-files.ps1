372623c7eef4aaee8180b520be0ff1c14e34e513
Kyle Rode <kyle@mytech.today>
2023-03-31 18:31:38 -0500

    modified:   JsonUrl.json 	modified:   download-files.ps1

--- download-files.ps1
+++ download-files.ps1
@@ -1,12 +1,11 @@
 <#
-
 .SYNOPSIS
 This is a PowerShell script that downloads files from a JSON file using the URLs provided in the JSON, and saves them in a specified directory.
 
 .EXAMPLE
-.\\download-files.ps1 -JsonUrl "https://insidiousmeme.com/presenta/ai/powershell/test.json" -OutputDir "C:\MyDownloads" -Verbose
+.\\download-files.ps1 -JsonUrl "https://insidiousmeme.com/presenta/ai/powershell/test.json" -Verbose
 
-This example downloads files from the JSON file located at https://example.com/files.json and saves them to the directory C:\MyDownloads.
+This example downloads files from the JSON file located at https://example.com/files.json and saves them to the directory $env:Programs\myTechToday\downloads\{today's date in yyyy-mm-dd format} if it exists, otherwise it checks if the myTech.Today System Environment Variable is set, and if so it will use {path of that variable}\downloads\{today's date in yyyy-mm-dd format}\ as the destination path for the files. Otherwise, it will use the current working directory.
 
 .PARAMETER JsonUrl
 The URL of the JSON file containing the download URLs.  JsonUrl file in the format of the following:
@@ -35,21 +34,18 @@ The URL of the JSON file containing the download URLs.  JsonUrl file in the form
         },
         {
             "Name": "myTech.Today Tools.url",
-            "Url": "https://insidiousmeme.com/presenta/ai/powershell/myTech.TodayTools.url"
+            "Url\": "https://insidiousmeme.com/presenta/ai/powershell/myTech.TodayTools.url"
         },
         {
             "Name": "myTechToday-portrait-02.ico",
-            "Url": "https://insidiousmeme.com/presenta/ai/powershell/myTechToday-portrait-02.ico"
+            "Url\": \"https://insidiousmeme.com/presenta/ai/powershell/myTechToday-portrait-02.ico"
         },
         {
-            "Name": "myTech.Today.url",
+            "Name": "myTech.Today.url\",
             "Url": "https://insidiousmeme.com/presenta/ai/powershell/myTech.Today.url"
         }
     ]
 }
-
-.PARAMETER OutputDir
-The directory where the downloaded files will be saved. If not specified, it will use the directory specified in the environment variable "myTechToday" or MyDocuments\Downloads if the variable is not set.
 
 .OUTPUTS
 This script does not return any objects, but writes downloaded files to the specified directory.
@@ -61,39 +57,32 @@ This script requires PowerShell 3.0 or higher.
 [CmdletBinding()]
 param (
     [Parameter(Mandatory = $true)]
-    [string]$JsonUrl,
-    [Parameter()]
-    [string]$OutputDir
+    [string]$JsonUrl
 )
-try {
-    # If the $OutputDir is null, use environment variable. 
-    if (!$OutputDir) {
-        # check if myTechToday is set 
-        if ($myTechToday = [Environment]::GetEnvironmentVariable("myTechToday")) {
-            $today = Get-Date -format "yyyy-MM-dd"
-            # Check and create  directory
-            $OutputDir = Join-Path $myTechToday "downloads\$today"
-        } 
-        else {
-            # Create the directory if it doesn't already exist
-            $downloadsDir = [Environment]::GetFolderPath("Public") + "\myTechToday\downloads\"            
-            if (!(Test-Path -Path $downloadsDir)) {
-                New-Item -ItemType Directory -Path $downloadsDir
-            }
-            # Create and test date specific dir
-            $today = Get-Date -format "yyyy-MM-dd"
-            $OutputDir = Join-Path $downloadsDir $today
-            if (!(Test-Path -Path $OutputDir)) {
-                New-Item -ItemType Directory -Path $OutputDir
-            }
 
-        }
-    } else {
-        #check and Create the directory, if it doesn't already exist
-        if (!(Test-Path -Path $OutputDir -PathType Container)) {
-            New-Item -ItemType Directory -Path $OutputDir
-        }
-    }
+$today = Get-Date -format "yyyy-MM-dd"
+
+# Set the default output directory to the programs directory or the value of the environmental variable
+$programDir = "$env:Programs\myTechToday\downloads\"
+if ([Environment]::GetEnvironmentVariable("myTechToday")) {
+    $customDir = [Environment]::GetEnvironmentVariable("myTechToday") + "\downloads\"
+}
+
+# Check if custom directory exists and set output directory accordingly
+if ($customDir -and (Test-Path -Path $customDir)) {
+    $OutputDir = Join-Path $customDir $today
+} elseif (Test-Path -Path $programDir) {
+    $OutputDir = Join-Path $programDir $today
+} else {
+    Write-Warning "Neither custom nor default directory found. Using current working directory."
+    $OutputDir = Get-Location
+}# If the download directory does not exist, create it
+if (!(Test-Path -Path $OutputDir)) {
+    Write-Verbose "Creating directory: $OutputDir"
+    New-Item -ItemType Directory -Path $OutputDir
+}
+
+try {
     # Check if the web server is accessible, Check if the $json object is null or if it does not contain files array. 
     $json = Invoke-RestMethod $JsonUrl
     if (!$json.Files) {
@@ -107,15 +96,15 @@ try {
             
             # Test if the file exists on the internet
             $fileAvailable = $true
-            try {
+            try {                
                 $null = Invoke-WebRequest $fileUrl -UseBasicParsing -ErrorAction Stop
             } catch {
                 $fileAvailable = $false
             }
 
             # If the file exists on the internet, download it, otherwise print a warning message
-            if ($fileAvailable) {
-                # Testing if the file exists already
+            if ($fileAvailable) {                
+                # Test if the file already exists
                 if (!(Test-Path -Path $fileOutputPath)) {
                     # Download file to $OutputDir.                    
                     Invoke-WebRequest $fileUrl -OutFile $fileOutputPath
@@ -124,19 +113,19 @@ try {
                         Write-Warning "Download of $fileUrl to $fileOutputPath failed!"
                     } else {
                         Write-Verbose "Downloaded $fileName successfully from $fileUrl to $fileOutputPath"
-                    }
-                } else {
+                    }                } else {
                     Write-Warning "File $fileName already exists in $OutputDir"
                 }
             } else {
-                Write-Warning "File $fileName unavailable for download from $fileUrl. Skipping...\"            }
+                Write-Warning "File $fileName unavailable for download from $fileUrl. Skipping...\\\"
+            }
         }
     }
-}
-catch [System.Exception]{
-    Write-Error "An error occurred: $($_.Exception.Message)"
+} catch {
+    Write-Error "An error occurred: $($Error[0].Exception.Message)"
     exit 1
 }
 
 # Return success.
 exit 0
+```
