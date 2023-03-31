9b131e784d43782c147da62dc0f264ac16657c26
Kyle Rode <kyle@mytech.today>
2023-03-31 17:47:54 -0500

    modified:   download-files.ps1

--- download-files.ps1
+++ download-files.ps1
@@ -1,6 +1,4 @@
-<#
-
-.SYNOPSIS
+<#.SYNOPSIS
 This is a PowerShell script that downloads files from a JSON file using the URLs provided in the JSON, and saves them in a specified directory.
 
 .EXAMPLE
@@ -9,45 +7,43 @@ This is a PowerShell script that downloads files from a JSON file using the URLs
 This example downloads files from the JSON file located at https://example.com/files.json and saves them to the directory C:\MyDownloads.
 
 .PARAMETER JsonUrl
-The URL of the JSON file containing the download URLs.  
-
-JsonUrl file in the format of the following:
+The URL of the JSON file containing the download URLs.  JsonUrl file in the format of the following:
 
 {
-  "Files": [
-    {
-      "Name": "chatgpt-purple.ico\",
-      "Url": "https://insidiousmeme.com/presenta/ai/powershell/chatgpt-purple.ico"
-    },
-    {
-      "Name": "ChatGPT.url",
-      "Url": "https://insidiousmeme.com/presenta/ai/powershell/ChatGPT.url"
-    },
-    {
-      "Name": "yakGPT.ico",
-      "Url": "https://insidiousmeme.com/presenta/ai/powershell/yakGPT.ico"
-    },
-    {
-      "Name": "YakGPT.url",
-      "Url": "https://insidiousmeme.com/presenta/ai/powershell/YakGPT.url"
-    },
+    "Files": [
         {
-      "Name": "tools.ico",
-      "Url": "https://insidiousmeme.com/presenta/ai/powershell/yakGPT.ico"
-    },
-    {
-      "Name": "myTech.Today Tools.url",
-      "Url": "https://insidiousmeme.com/presenta/ai/powershell/myTech.TodayTools.url"
-    },
-    {
-      "Name": "tools.ico",
-      "Url": "https://insidiousmeme.com/presenta/ai/powershell/myTechToday-portrait-02.ico"
-    },
-    {
-      "Name": "myTech.Today.url",
-      "Url": "https://insidiousmeme.com/presenta/ai/powershell/myTech.Today.url"
-    }
-  ]
+            "Name": "chatgpt-purple.ico",
+            "Url": "https://insidiousmeme.com/presenta/ai/powershell/chatgpt-purple.ico"
+        },
+        {
+            "Name": "ChatGPT.url",
+            "Url": "https://insidiousmeme.com/presenta/ai/powershell/ChatGPT.url"
+        },
+        {
+            "Name": "yakGPT.ico",
+            "Url": "https://insidiousmeme.com/presenta/ai/powershell/yakGPT.ico"
+        },
+        {
+            "Name": "YakGPT.url",
+            "Url": "https://insidiousmeme.com/presenta/ai/powershell/YakGPT.url"
+        },
+        {
+            "Name": "tools.ico",
+            "Url": "https://insidiousmeme.com/presenta/ai/powershell/tools.ico"
+        },
+        {
+            "Name": "myTech.Today Tools.url",
+            "Url": "https://insidiousmeme.com/presenta/ai/powershell/myTech.TodayTools.url"
+        },
+        {
+            "Name": "myTechToday-portrait-02.ico",
+            "Url": "https://insidiousmeme.com/presenta/ai/powershell/myTechToday-portrait-02.ico"
+        },
+        {
+            "Name": "myTech.Today.url",
+            "Url": "https://insidiousmeme.com/presenta/ai/powershell/myTech.Today.url"
+        }
+    ]
 }
 
 .PARAMETER OutputDir
@@ -61,7 +57,6 @@ This script requires PowerShell 3.0 or higher.
 #>
 
 [CmdletBinding()]
-
 param (
     [Parameter(Mandatory = $true)]
     [string]$JsonUrl,
@@ -69,17 +64,16 @@ param (
     [string]$OutputDir
 )
 
-# If the $OutputDir is null, use "myTechToday" environment variable as the directory. If "myTechToday" variable does not exist, use MyDocuments\Downloads.
-if (!$OutputDir) {
-    if ($myTechToday = [Environment]::GetEnvironmentVariable("myTechToday")) {
-        $today = Get-Date -format "yyyy-MM-dd"
-        $OutputDir = Join-Path $myTechToday "downloads\$today"
-    } else {
-        $OutputDir = [Environment]::GetFolderPath("MyDocuments") + "\Downloads"
-    }
-}
-
 try {
+    # If the $OutputDir is null, use "myTechToday" environment variable as the directory. If "myTechToday" variable does not exist, use MyDocuments\Downloads.
+    if (!$OutputDir) {
+        if ($myTechToday = [Environment]::GetEnvironmentVariable("myTechToday")) {
+            $today = Get-Date -format "yyyy-MM-dd"
+            $OutputDir = Join-Path $myTechToday "downloads\$today"
+        } else {
+            $OutputDir = [Environment]::GetFolderPath("MyDocuments") + "\Downloads"
+        }
+    }
 
     # Check if $OutputDir exists, otherwise create it.
     if (-not (Test-Path $OutputDir -PathType Container)) {
@@ -88,38 +82,42 @@ try {
     }
 
     # Make web request and store the JSON data in $files variable
-    $webRequest = [System.Net.WebRequest]::Create($JsonUrl)
-    $webResponse = $webRequest.GetResponse()
-    $jsonStream = $webResponse.GetResponseStream()
+    $json = Invoke-RestMethod $JsonUrl
 
-    $jsonReader = New-Object System.IO.StreamReader($jsonStream)
-    $json = $jsonReader.ReadToEnd()
-    $jsonReader.Close()
-
-    $filesObject = ConvertFrom-Json $json
-
-    # Check if the $files object is null, if so output an error message.
-    if (!$filesObject) {
+    # Check if the $json object is null or if it does not contain files array, if so output an error message.
+    if (!$json.Files) {
         Write-Error "Error: JSON file may be empty or incorrectly formatted."
     }
-    #if it exists test to see if a file si there first then download if it doesn't exist
-    else {        
-        foreach ($file in $filesObject.Files) {
+    else {
+        foreach ($file in $json.Files) {
 
             $fileName = $file.Name
             $fileUrl = $file.Url
             $fileOutputPath = Join-Path $OutputDir $fileName
             
-            # Testing if the file exists already
-            If (!(Test-Path -Path $fileOutputPath)) {
-                # Download file to $OutputDir.
-                Invoke-WebRequest $fileUrl -OutFile $fileOutputPath
-                 # To ensure that the file got downloaded successfully, test and validate the file again after downloading and report any errors
-                If(!(Test-Path -Path $fileOutputPath)){Throw "Download of $fileUrl to $fileOutputPath failed!"}
-                Write-Verbose "Downloaded $fileName successfully from $fileUrl to $fileOutputPath"
+            # Test if the file exists on the internet\n            $fileAvailable = $true
+            try {
+                $null = Invoke-WebRequest $fileUrl -UseBasicParsing -ErrorAction Stop
+            } catch {
+                $fileAvailable = $false
             }
-            else{
-                Write-Warning "File $fileName already exists in $OutputDir"
+
+            # If the file exists on the internet, download it, otherwise print a warning message
+            if ($fileAvailable) {
+                # Testing if the file exists already
+                if (!(Test-Path -Path $fileOutputPath)) {
+                    # Download file to $OutputDir.\n                    Invoke-WebRequest $fileUrl -OutFile $fileOutputPath
+                    # Test and validate the file after downloading to ensure it was downloaded successfully
+                    if (!(Test-Path -Path $fileOutputPath)) {
+                        Write-Warning "Download of $fileUrl to $fileOutputPath failed!"
+                    } else {
+                        Write-Verbose "Downloaded $fileName successfully from $fileUrl to $fileOutputPath"
+                    }
+                } else {
+                    Write-Warning "File $fileName already exists in $OutputDir"
+                }
+            } else {
+                Write-Warning "File $fileName unavailable for download from $fileUrl. Skipping..."
             }
         }
     }
